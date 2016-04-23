c3cd309a328dc7751353b956a5bdd91db9698b52
David Falcón Naranjo <david_el_125@hotmail.com>
2016-04-23 13:48:26 +0000

    single columns check try

--- src/game/Heuristics.py
+++ src/game/Heuristics.py
@@ -1,51 +1,22 @@
+def checkIfExistsAnotherSimilarTokenInColumn(state, key):
+    for rowIndex in range(1,7):
+        if rowIndex != key[0]:
+            if state.board.get(rowIndex,key[1]) == state.to_move:
+                return True
+    return False
 
-def checkPerimeter(state,key):
-    value = 0
-    if state.board.get((key[0]-1,key[1])) != None:
-        if state.board.get((key[0]-1,key[1])) == "X":
-            value += 5
-        else:
-            value += -3
-    if state.board.get((key[0]+1,key[1])) != None:
-        if state.board.get((key[0]+1,key[1])) == "X":
-            value += 5
-        else:
-            value += -3
-    if state.board.get((key[0],key[1]-1)) != None:
-        if state.board.get((key[0],key[1]-1)) == "X":
-            value += 5
-        else:
-            value += -3
-    if state.board.get((key[0],key[1]+1)) != None:
-        if state.board.get((key[0],key[1]+1)) == "X":
-            value += 5
-        else:
-            value += -3
-    if state.board.get((key[0]-1,key[1]+1)) != None:
-        if state.board.get((key[0]-1,key[1]+1)) == "X":
-            value += 10
-        else:
-            value += -6
-    if state.board.get((key[0]+1,key[1]-1)) != None:
-        if state.board.get((key[0]+1,key[1]-1)) == "X":
-            value += 10
-        else:
-            value += -6
-    if state.board.get((key[0]+1,key[1]+1)) != None:
-        if state.board.get((key[0]+1,key[1]+1)) == "X":
-            value += 10
-        else:
-            value += -6
-    if state.board.get((key[0]-1,key[1]-1)) != None:
-        if state.board.get((key[0]-1,key[1]-1)) == "X":
-            value += 10
-        else:
-            value += -6
-    return value
+
+def checkSingleToken(state, key):
+    accumulatedHeuristicsValue = 1
+    if checkIfExistsAnotherSimilarTokenInColumn(state,key):
+        accumulatedHeuristicsValue += 50
+    return accumulatedHeuristicsValue
 
 def firstHeuristics(state):
     heuristicsValue = 0
     for key in state.board:
-        if state.board[key] == "X":
-            #heuristicsValue += checkPerimeter(state,key)
-            return checkPerimeter(state,key)
+        if state.board[key] == state.to_move:
+                heuristicsValue += checkSingleToken(state, key)
+    return heuristicsValue
+
+
