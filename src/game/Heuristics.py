bdd6af1786ce78e299759f193555688c63f2ca6f
David Falcón Naranjo <david_el_125@hotmail.com>
2016-04-24 11:23:25 +0000

    Vertical connection works

--- src/game/Heuristics.py
+++ src/game/Heuristics.py
@@ -1,22 +1,25 @@
-def checkIfExistsAnotherSimilarTokenInColumn(state, key):
-    for rowIndex in range(1,7):
-        if rowIndex != key[0]:
-            if state.board.get(rowIndex,key[1]) == state.to_move:
-                return True
-    return False
-
-
-def checkSingleToken(state, key):
-    accumulatedHeuristicsValue = 1
-    if checkIfExistsAnotherSimilarTokenInColumn(state,key):
-        accumulatedHeuristicsValue += 50
-    return accumulatedHeuristicsValue
+def checkVerticalConnection(state, key):
+    accumulatedValue = 0
+    if state.board.get((key[0], key[1])) == state.to_move:
+        if state.board.get((key[0], key[1]+1)) == state.to_move:
+            accumulatedValue += 5
+        if state.board.get((key[0], key[1]+2)) == state.to_move:
+            accumulatedValue += 10
+        if state.board.get((key[0], key[1]+3)) == state.to_move:
+             accumulatedValue += 1000
+    if state.board.get((key[0], key[1])) == "O":
+        if state.board.get((key[0], key[1]+1)) == "O":
+            accumulatedValue -= 8
+        if state.board.get((key[0], key[1]+2)) == "O":
+            accumulatedValue -= 20
+        if state.board.get((key[0], key[1]+3)) == "O":
+            accumulatedValue -= 500
+    return accumulatedValue
 
 def firstHeuristics(state):
     heuristicsValue = 0
     for key in state.board:
-        if state.board[key] == state.to_move:
-                heuristicsValue += checkSingleToken(state, key)
+        heuristicsValue += checkVerticalConnection(state, key)
     return heuristicsValue
 
 
