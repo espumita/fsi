ea5ba0fc63e8d1804435905f169eacd0c6b926ba
David Falcón Naranjo <david_el_125@hotmail.com>
2016-04-27 16:37:28 +0000

    UpwardsDiagonal

--- src/game/Heuristics.py
+++ src/game/Heuristics.py
@@ -40,6 +40,29 @@ def checkHorizontalConnection(state, key):
                 return -5
     return 0
 
+
+def checkUpwardsDiagonal(state, key):
+    if state.board.get((key[0], key[1])) == "X":
+        if state.board.get((key[0]+1, key[1]+1)) == "X":
+            if state.board.get((key[0]+2, key[1]+2)) == "X":
+                if state.board.get((key[0]+3, key[1]+3)) == "X":
+                    return float('inf')
+                else:
+                    return 10
+            else:
+                return 5
+    else:
+        if state.board.get((key[0]+1, key[1]+1)) == "O":
+            if state.board.get((key[0]+2, key[1]+2)) == "O":
+                if state.board.get((key[0]+3, key[1]+3)) == "O":
+                    return -float('inf')
+                else:
+                    return -10
+            else:
+                return -5
+    return 0
+
+
 def firstHeuristics(state):
     heuristicsValue = 0
     for key in state.board:
@@ -49,6 +72,11 @@ def firstHeuristics(state):
         if heuristicsValue == float('inf'):
             return float('inf')
         heuristicsValue += checkHorizontalConnection(state, key)
+        if heuristicsValue == -float('inf'):
+            return -float('inf')
+        if heuristicsValue == float('inf'):
+            return float('inf')
+        heuristicsValue += checkUpwardsDiagonal(state, key)
         if heuristicsValue == -float('inf'):
             return -float('inf')
         if heuristicsValue == float('inf'):
