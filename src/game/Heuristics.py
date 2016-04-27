b3dfc6a43bd7d8eadabb81edeb52e3d80e347372
David Falcón Naranjo <david_el_125@hotmail.com>
2016-04-27 16:22:57 +0000

    accumulated value has been removed

--- src/game/Heuristics.py
+++ src/game/Heuristics.py
@@ -1,56 +1,44 @@
 def checkVerticalConnection(state, key):
-    accumulatedValue = 0
     if state.board.get((key[0], key[1])) == "X":
         if state.board.get((key[0], key[1]+1)) == "X":
-            accumulatedValue += 5
             if state.board.get((key[0], key[1]+2)) == "X":
-                accumulatedValue += 10
                 if state.board.get((key[0], key[1]+3)) == "X":
                     return float('inf')
                 else:
-                    accumulatedValue = 4
+                    return 10
             else:
-                accumulatedValue = 14
+                return 5
     else:
         if state.board.get((key[0], key[1]+1)) == "O":
-            accumulatedValue -= 5
             if state.board.get((key[0], key[1]+2)) == "O":
-                accumulatedValue -= 10
                 if state.board.get((key[0], key[1]+3)) == "O":
                     return -float('inf')
                 else:
-                    accumulatedValue = 4
+                    return -10
             else:
-                accumulatedValue = 14
-    return accumulatedValue
-
+                return -5
+    return 0
 
 def checkHorizontalConnection(state, key):
-    accumulatedValue = 0
     if state.board.get((key[0], key[1])) == "X":
         if state.board.get((key[0]+1, key[1])) == "X":
-            accumulatedValue += 5
             if state.board.get((key[0]+2, key[1])) == "X":
-                accumulatedValue += 10
                 if state.board.get((key[0]+3, key[1])) == "X":
                     return float('inf')
                 else:
-                    accumulatedValue = 4
+                    return 10
             else:
-                accumulatedValue = 14
+                return 5
     else:
         if state.board.get((key[0]+1, key[1])) == "O":
-            accumulatedValue -= 5
             if state.board.get((key[0]+2, key[1])) == "O":
-                accumulatedValue -= 10
                 if state.board.get((key[0]+3, key[1])) == "O":
                     return -float('inf')
                 else:
-                    accumulatedValue = 4
+                    return -10
             else:
-                accumulatedValue = 14
-    return accumulatedValue
-
+                return -5
+    return 0
 
 def firstHeuristics(state):
     heuristicsValue = 0
