851709c914f0198684e8e6fc88dfac632c0e78cf
David Falcón Naranjo <david_el_125@hotmail.com>
2016-04-28 17:30:35 +0100

    Now the bot starts in the center of the board

--- src/game/Heuristics.py
+++ src/game/Heuristics.py
@@ -5,18 +5,30 @@ def checkVerticalConnection(state, key):
                 if state.board.get((key[0], key[1]+3)) == "X":
                     return float('inf')
                 else:
-                    return 10
+                    if (key[0],key[1]+3) in state.moves:
+                        return 25
+                    else:
+                        return 0
             else:
-                return 5
+                if (key[0],key[1]+2) in state.moves:
+                    return 5
+                else:
+                    return 0
     else:
         if state.board.get((key[0], key[1]+1)) == "O":
             if state.board.get((key[0], key[1]+2)) == "O":
                 if state.board.get((key[0], key[1]+3)) == "O":
                     return -float('inf')
                 else:
-                    return -10
+                    if (key[0],key[1]+3) in state.moves:
+                        return -25
+                    else:
+                        return 0
             else:
-                return -5
+                if (key[0],key[1]+2) in state.moves:
+                    return -5
+                else:
+                    return 0
     return 0
 
 def checkHorizontalConnection(state, key):
@@ -26,18 +38,30 @@ def checkHorizontalConnection(state, key):
                 if state.board.get((key[0]+3, key[1])) == "X":
                     return float('inf')
                 else:
-                    return 10
+                    if (key[0]+3,key[1]) in state.moves:
+                        return 30
+                    else:
+                        return 0
             else:
-                return 5
+                if (key[0]+2,key[1]) in state.moves:
+                    return 8
+                else:
+                    return 0
     else:
         if state.board.get((key[0]+1, key[1])) == "O":
             if state.board.get((key[0]+2, key[1])) == "O":
                 if state.board.get((key[0]+3, key[1])) == "O":
                     return -float('inf')
                 else:
-                    return -10
+                    if (key[0]+3,key[1]) in state.moves:
+                        return -30
+                    else:
+                        return 0
             else:
-                return -5
+                if (key[0]+2,key[1]) in state.moves:
+                    return -8
+                else:
+                    return 0
     return 0
 
 
@@ -48,18 +72,30 @@ def checkUpwardsDiagonal(state, key):
                 if state.board.get((key[0]+3, key[1]+3)) == "X":
                     return float('inf')
                 else:
-                    return 10
+                    if (key[0]+3,key[1]+3) in state.moves:
+                        return 45
+                    else:
+                        return 0
             else:
-                return 5
+                if (key[0]+2,key[1]+2) in state.moves:
+                    return 12
+                else:
+                    return 0
     else:
         if state.board.get((key[0]+1, key[1]+1)) == "O":
             if state.board.get((key[0]+2, key[1]+2)) == "O":
                 if state.board.get((key[0]+3, key[1]+3)) == "O":
                     return -float('inf')
                 else:
-                    return -10
+                    if (key[0]+3,key[1]+3) in state.moves:
+                        return -45
+                    else:
+                        return 0
             else:
-                return -5
+                if (key[0]+2,key[1]+2) in state.moves:
+                    return -12
+                else:
+                    return 0
     return 0
 
 
@@ -70,18 +106,30 @@ def checkDownwardDiagonal(state, key):
                 if state.board.get((key[0]+3, key[1]-3)) == "X":
                     return float('inf')
                 else:
-                    return 10
+                    if (key[0]+3,key[1]-3) in state.moves:
+                        return 45
+                    else:
+                        return 0
             else:
-                return 5
+                if (key[0]+2,key[1]-2) in state.moves:
+                    return 12
+                else:
+                    return 0
     else:
         if state.board.get((key[0]+1, key[1]-1)) == "O":
             if state.board.get((key[0]+2, key[1]-2)) == "O":
                 if state.board.get((key[0]+3, key[1]-3)) == "O":
                     return -float('inf')
                 else:
-                    return -10
+                    if (key[0]+3,key[1]-3) in state.moves:
+                        return -45
+                    else:
+                        return 0
             else:
-                return -5
+                if (key[0]+2,key[1]-2) in state.moves:
+                    return -12
+                else:
+                    return 0
     return 0
 
 def firstHeuristics(state):
@@ -97,12 +145,12 @@ def firstHeuristics(state):
             return -float('inf')
         if heuristicsValue == float('inf'):
             return float('inf')
-        heuristicsValue += checkUpwardsDiagonal(state, key)
+        #heuristicsValue += checkUpwardsDiagonal(state, key)
         if heuristicsValue == -float('inf'):
             return -float('inf')
         if heuristicsValue == float('inf'):
             return float('inf')
-        heuristicsValue += checkDownwardDiagonal(state, key)
+        #heuristicsValue += checkDownwardDiagonal(state, key)
         if heuristicsValue == -float('inf'):
             return -float('inf')
         if heuristicsValue == float('inf'):
