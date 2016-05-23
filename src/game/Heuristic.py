fbd541eba2c33421213495aeef6b2175757fa982
David Falcón Naranjo <david_el_125@hotmail.com>
2016-05-23 12:25:17 +0000

    Memoization optimization technique

--- src/game/Heuristic.py
+++ src/game/Heuristic.py
@@ -4,6 +4,19 @@ from game.heuristics.UpwardHeuristic import UpwardHeuristic
 from game.heuristics.DownwardHeuristic import DownwardHeuristic
 
 
+def memoization(function_call):
+    cached_buffer = {}
+
+    def helper(state, problem_player):
+        key = str(state.board)
+        if key not in cached_buffer:
+            cached_buffer[key] = function_call(state, problem_player)
+        return cached_buffer[key]
+
+    return helper
+
+
+@memoization
 def heuristic(state, problem_player):
     heuristic_value = 0
     other_player = "O" if problem_player == "X" else "X"
